0a191d0093d252261ccc52f1c209a23639efd095
doug.w <doug.wright@controlgap.com>
2020-07-16 16:20:36 -0400

    initial commit

---
+++ mdns-test.js
@@ -0,0 +1,39 @@
+var bonjour = require("bonjour")();
+var axios = require("axios");
+
+function discover() {
+  let lightStrips = [];
+  bonjour.find({ type: "http" }, (service) => {
+    if (service.name.includes("led-strip")) {
+      //   console.log("Found a led-strip:", service);
+      lightStrips.push(service);
+    }
+  });
+
+  return new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      bonjour.destroy();
+      let discoveredDevices = [];
+      for (let i = 0; i < lightStrips.length; i++) {
+        let resp = await axios.get(`http://${lightStrips[i].referer.address}`);
+
+        let [type, mac, serial] = resp.data.split("\n");
+
+        discoveredDevices.push({
+          type,
+          mac,
+          serial,
+          address: lightStrips[i].referer.address,
+          hostname: lightStrips[i].host,
+          uniqueName: lightStrips[i].name,
+        });
+      }
+
+      resolve(discoveredDevices);
+    }, 10000);
+  });
+}
+
+(async () => {
+  console.log(await discover());
+})();
--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,10 @@
+/* eslint-disable quotes */
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
+export const PLATFORM_NAME = "LightStripPlugin";
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-plugin-name';
+export const PLUGIN_NAME = "lightstrip-hb-plugin";
